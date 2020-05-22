90796c9fcf0f8e4873b26a9f7a425362c0f41f30
Kobatai <taiga1983346@gmail.com>
2020-05-22 09:51:35 +0900

    finish: to Slice

--- server/main.go
+++ server/main.go
@@ -11,6 +11,7 @@ import (
 	// "math/cmplx"
 	// "runtime"
 	// "time"
+	// "strings"
 )
 
 type templateHandler struct {
@@ -66,17 +67,17 @@ func sqrt(x float64) string {
 	return fmt.Sprint(math.Sqrt(x))
 }
 
-func pow(x, n, lim float64) float64 {
-	// Pow = べき乗
-	// 条件で評価する前にステートメントを記述できる
-	// xをnべき乗したものとlimを比較
-	if v := math.Pow(x, n); v < lim {
-		return v
-	} else {
-		fmt.Printf("%g >= %g\n", v, lim)
-	}
-	return lim
-}
+// func pow(x, n, lim float64) float64 {
+// 	// Pow = べき乗
+// 	// 条件で評価する前にステートメントを記述できる
+// 	// xをnべき乗したものとlimを比較
+// 	if v := math.Pow(x, n); v < lim {
+// 		return v
+// 	} else {
+// 		fmt.Printf("%g >= %g\n", v, lim)
+// 	}
+// 	return lim
+// }
 
 func Sqrt(x float64) float64 {
 	// z := float64(1)
@@ -111,6 +112,20 @@ const (
 	// Shift it right again 99 places, so we end up with 1<<1, or 2.
 	Small = Big >> 99
 )
+
+// クラスがないため構造体のstructが似た役割として使われる
+type Vertex struct {
+	X, Y int
+}
+
+var (
+	v1 = Vertex{1, 2}  // has type Vertex
+	v2 = Vertex{X: 1}  // Y:0 is implicit
+	v3 = Vertex{}      // X:0 and Y:0
+	p  = &Vertex{1, 2} // has type *Vertex
+)
+
+// var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
 
 func main() {
 	// ・Packages, variables, and functions.
@@ -257,12 +272,207 @@ func main() {
 	// defer fmt.Println("world")
 	// fmt.Println("hello")
 
-	// deferへ渡した関数が複数あればスタックされ、last in first outで実行される(入れたもの順に実行される)
-	fmt.Println("counting")
+	// deferへ渡した関数が複数あればスタックされ、last in first out(LFIO)で実行される(入れたもの順に実行される)
+	// fmt.Println("counting")
 
-	for i := 0; i < 10; i++ {
-		defer fmt.Println(i)
+	// for i := 0; i < 10; i++ {
+	// 	defer fmt.Println(i)
+	// }
+
+	// fmt.Println("done")
+
+	// i, j := 42, 2701
+	// &はオペランドのポインタを引き出す
+	// オぺランドは演算の種類や内容を表す演算子（オペレータ）によって演算対象となるもの
+	// p := &i
+	// fmt.Println(p)
+	// *はpのポインタを通してその中身をみる
+	// fmt.Println(*p)
+	// ポインタを通してiに値を代入
+	// *p = 21
+	// fmt.Println(i)
+
+	// p = &j
+	// fmt.Println(p)
+	// *p = *p / 37
+	// fmt.Println(j)
+	// "dereferencing" または "indirectingという
+
+	// fmt.Println(Vertex{1, 2})
+
+	// .でstructにアクセスできる
+	// v := Vertex{1, 2}
+	// v.X = 4
+	// fmt.Println(v.X)
+
+	// v := Vertex{1, 2}
+	// structのフィールドはポインタを通してもアクセス可能
+	// p := &v
+	// (*p).X *Pの中身のXという書き方もできるが.で省略可能
+	// p.X = 1e9
+	// fmt.Println(v)
+
+	// fmt.Println(v1, p, v2, v3)
+
+	// 配列は中身が何個であるか制約できる
+	// var a [2]string
+	// a[0] = "Hello"
+	// a[1] = "World"
+	// fmt.Println(a[0], a[1])
+	// fmt.Println(a)
+
+	// primes := [6]int{2, 3, 5, 7, 11, 13}
+	// fmt.Println(primes)
+
+	// 数を指定しなければスライス　可変長で入れられる
+	// a[low:high]で配列のlowからhighの差分取り出せる
+	// この場合1~3まで
+	// primes := [6]int{2, 3, 5, 7, 11, 13}
+	// var s []int = primes[1:4]
+	// fmt.Println(s)
+
+	// names := [4]string{
+	// 	"John",
+	// 	"Paul",
+	// 	"George",
+	// 	"Ringo",
+	// }
+	// fmt.Println(names)
+
+	// スライスは実際に値を保持しているのではなく元の配列を参照している
+	// そのためスライスの値を変えれば元の値が変わる
+	// a := names[0:2]
+	// b := names[1:3]
+	// fmt.Println(a, b)
+
+	// b[0] = "XXX"
+	// fmt.Println(a, b)
+	// fmt.Println(names)
+
+	// スライスだけ作成すればそれを参照している配列も同時に作成される
+	// q := []int{2, 3, 5, 7, 11, 13}
+	// fmt.Println(q)
+
+	// r := []bool{true, false, true, true, false, true}
+	// fmt.Println(r)
+
+	// s := []struct {
+	// 	i int
+	// 	b bool
+	// }{
+	// 	{2, true},
+	// 	{3, false},
+	// 	{5, true},
+	// 	{7, true},
+	// 	{11, false},
+	// 	{13, true},
+	// }
+	// fmt.Println(s)
+
+	// スライスの規定値を利用すれば省略可
+	// s := []int{2, 3, 5, 7, 11, 13}
+	// 1~3
+	// s = s[1:4]
+	// fmt.Println(s)
+
+	// その場合既定値-1されない
+	// 1~2
+	// s = s[:2]
+	// fmt.Println(s)
+
+	// 1
+	// s = s[1:]
+	// fmt.Println(s)
+
+	// この容量内で再度スライスすればその中身はいくらでも取り出せる
+	// s := []int{2, 3, 5, 7, 11, 13}
+	// printSlice(s)
+
+	// Slice the slice to give it zero length.
+	// s = s[:0]
+	// printSlice(s)
+
+	// Extend its length.
+	// s = s[:4]
+	// printSlice(s)
+
+	// Drop its first two values.
+	// s = s[2:]
+	// printSlice(s)
+
+	// スライスに何も入れなければnil
+	// var s []int
+	// printSlice(s)
+	// if s == nil {
+	// 	fmt.Println("nil")
+	// }
+
+	// make 関数はゼロ化された配列を割り当て、その配列を指すスライスを返す
+	// a := make([]int, 5)
+	// printSlice("a", a)
+
+	// 2番目の引数はlen
+	// 3番目の引数にcapを指定できる
+	// b := make([]int, 0, 5)
+	// b = b[:cap(b)] // len(b)=5, cap(b)=5
+	// b = b[1:]      // len(b)=4, cap(b)=4
+	// printSlice("b", b)
+
+	// c := b[:2]
+	// printSlice("c", c)
+
+	// d := c[2:5]
+	// printSlice("d", d)
+
+	// board := [][]string{
+	// 	[]string{"_", "_", "_"},
+	// 	[]string{"_", "_", "_"},
+	// 	[]string{"_", "_", "_"},
+	// }
+
+	// fmt.Println("%v", board)
+
+	// board[0][0] = "X"
+	// board[0][2] = "X"
+	// board[1][0] = "O"
+	// board[1][2] = "X"
+	// board[2][2] = "O"
+
+	// for i := 0; i < len(board); i++ {
+	// 	fmt.Printf("%s\n", strings.Join(board[i], " "))
+	// }
+
+	// 要素の追加はappend
+	// var s []int
+	// printSlice(s)
+
+	// s = append(s, 0)
+	// printSlice(s)
+
+	// s = append(s, 1)
+	// printSlice(s)
+
+	// s = append(s, 2, 3, 4)
+	// printSlice(s)
+
+	// rangeはforeachみたいなもの　値を一つずつ取り出し処理
+	// その時indexとindexの場所の要素のコピーを返す　indexとvalue
+	// for i, v := range pow {
+	// 	fmt.Printf("2**%d = %d\n", i, v)
+	// }
+
+	// indexやvalueはいらなければ、indexは _ もしくは valueは省略可
+	pow := make([]int, 10)
+	for i := range pow {
+		pow[i] = 1 << uint(i)
+	}
+	for _, value := range pow {
+		fmt.Printf("%d\n", value)
 	}
 
-	fmt.Println("done")
+}
+
+func printSlice(s []int) {
+	// lenは要素の長さ capはあとどれだけ入れられるか
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
 }
